d2652ea9f2bc53d043d75925290d4107ea082e48
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-03-29 14:20:54 +0400

    edited class books with all attributes

--- Books.java
+++ Books.java
@@ -2,12 +2,49 @@ import java.util.Scanner;
 
 public class Books {
 
-
+    private int id;
     private String title;
     private String author;
+    private Boolean isBorrowed;
 
-    private String lastName;
+    public Books(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isBorrowed = false;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Boolean getBorrowed() {
+        return isBorrowed;
+    }
+
+    public void setBorrowed(Boolean borrowed) {
+        isBorrowed = borrowed;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
