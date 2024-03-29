0897ce741887a9ba71d53eaa5b34bceab717636f
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-03-29 14:17:18 +0400

    created books class

---
+++ Books.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Books {
+
+
+    private String title;
+    private String author;
+
+    private String lastName;
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
+
