032898f288f3df8416ca527b728becf166ec302d
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-03-29 15:48:06 +0400

    made the code a lot simpler and added comments

--- Book.java
+++ Book.java
@@ -1,66 +1,34 @@
 
 public class Book {
 
-    private int id;
     private String title;
     private String author;
     private Boolean isBorrowed;
 
-    public Book(int id, String title, String author) {
-        this.id = id;
+    public Book(String title, String author) {
         this.title = title;
         this.author = author;
         this.isBorrowed = false;
     }
 
-    public int getId() {
-        return id;
+    // to mark the book as borrowed
+    public void borrow() {
+        this.isBorrowed = true;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    // to mark the book as returned
+    public void returnBook(){
+        this.isBorrowed = false;
     }
 
-    public String getTitle() {
-        return title;
+    public Boolean isBorrowed() {
+        return this.isBorrowed;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public String toString(){
+        return title + " by " + author;
     }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public Boolean getBorrowed() {
-        return isBorrowed;
-    }
-
-    public void setBorrowed(Boolean borrowed) {
-        isBorrowed = borrowed;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id + "\n" +
-                "title='" + title + "\n" +
-                "author='" + author + "\n" +
-                "isBorrowed=" + isBorrowed +
-                '}';
-    }
-
-//    public static void main(String[] args) {
-//        Book book = new Book(27, "The Dark Truth", "Suly");
-//
-//        book.isBorrowed = true;
-//        System.out.println(book.toString());
-//    }
 
 }
+
 
