92f13838bc23273b57e2071be7ad268f67fc95ba
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-03-29 16:05:13 +0400

    complied the code to test for errors,, ALLGOOD

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args){
         Library library = new Library();
-        
+        Book book1 = new Book("The Dark", "J.R. MAck");
+
+        Borrower borrower1 = new Borrower("Suly alhudifi");
+
+        library.addBook(book1);
+        library.addBorrower(borrower1);
+
+        library.borrowBook(book1,borrower1);
+
+        library.returnBook(book1);
     }
 }
